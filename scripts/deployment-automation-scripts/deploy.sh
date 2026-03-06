8f9224ccebf9eead0b107e16f0b2b47bdfcb2828
Qays Alnajjad <103290698+QaysAlnajjad@users.noreply.github.com>
2026-03-06 23:54:24 +0400

    Modify deploy.sh to source config.yaml
    Updated source command to include config.yaml.

--- scripts/deployment-automation-scripts/deploy.sh
+++ scripts/deployment-automation-scripts/deploy.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 set -e
-source "$(dirname "$0")/load-config.sh"
+source "$(dirname "$0")/load-config.sh" "$(dirname "$0")/config.yaml"
 source "$(dirname "$0")/stacks_config.sh"
-
-
 
 
 # Validate TF backend bucket
