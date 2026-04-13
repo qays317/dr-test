3d3c18e7f2d699cbbfb772bf79a28d99cfb496a6
qays-test <qaysalnajjad317@gmail.com>
2026-04-14 01:39:08 +0400

    Refactor AWS provider configuration in providers.tf
    Removed unused archive provider and updated AWS provider region variable.

--- environments/operations/dr_orchestration/providers.tf
+++ environments/operations/dr_orchestration/providers.tf
@@ -1,22 +1,16 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = "~> 6.0.0"
-    }
-
-    archive = {
-      source  = "hashicorp/archive"
-      version = "~> 2.5"
     }
   }
 }
 
-provider "aws" {
-  region = var.dr_region
+variable "primary_region" {
+  type = string
 }
 
 provider "aws" {
-  alias  = "dr"
-  region = var.dr_region
+  region = var.primary_region
 }
