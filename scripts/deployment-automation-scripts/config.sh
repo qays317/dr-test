54a5131975c4f9dbaf7bbe62331d8efd3f16bfc7
qays-test <qaysalnajjad317@gmail.com>
2026-04-14 18:47:39 +0400

    Update config.sh

--- scripts/deployment-automation-scripts/config.sh
+++ scripts/deployment-automation-scripts/config.sh
@@ -28,6 +28,7 @@ DR_MEDIA_S3_BUCKET="wordpress-media-dr-20261"
 #  Media S3 buckets
 ############################################
 RDS_IDENTIFIER="wordpress-rds"
+RDS_IDENTIFIER="wordpress-rds-replica"
 MAX_REPLICA_LAG=30
 
 ############################################
