d579eda776460a39bc355fdc892f0597ae2cc663
qays-test <qaysalnajjad317@gmail.com>
2026-04-16 22:29:38 +0400

    Update outputs.tf

--- modules/sg/outputs.tf
+++ modules/sg/outputs.tf
@@ -24,6 +24,12 @@ output "vpc_endpoints_security_group_id" {
 
 output "db_setup_lambda_security_group_id" {
     value = try([ for k, v in aws_security_group.main : v.id 
-                  if lookup( v.tags, "Name", "") == "Lambda-SG"][0], null)
-  
+                  if lookup( v.tags, "Name", "") == "Lambda-SG"][0], null)  
 }
+
+output "dr_rds_sg_id" {
+    value = try([ for k, v in aws_security_group.main : v.id
+                  if lookup( v.tags, "Name", "") == "vpc-endpoints-SG"][0], null ) 
+}
+
+
