c615af343bdba6450a282fc33a45b9b8b5b71766
qays-test <qaysalnajjad317@gmail.com>
2026-04-16 22:40:08 +0400

    Update variables.tf

--- environments/dr/read_replica_rds/variables.tf
+++ environments/dr/read_replica_rds/variables.tf
@@ -5,3 +5,27 @@ variable "rds_identifier" {
 variable "rds_replica_identifier" {
   type = string
 }
+
+variable "security_group" {
+    type = map(object({
+        ingress = optional (map(object({
+            from_port = number
+            to_port = number
+            ip_protocol = string
+            cidr_block = optional(string)
+            vpc_cidr = optional(bool)
+            source_security_group_name = optional(string)
+            prefix_list_ids = optional (list(string))
+        })))
+        egress = optional(map(object({
+            from_port = number
+            to_port = number
+            ip_protocol = string
+            cidr_block = optional(string)
+            vpc_cidr = optional(bool)
+            source_security_group_name = optional(string)
+            prefix_list_ids = optional (list(string))
+        })))
+        tags = optional(map(string))
+    }))  
+}
