3cb290f4b0064266782a719b3605d09c6d9dcd5a
qays-test <qaysalnajjad317@gmail.com>
2026-04-17 14:07:20 +0400

    Update outputs.tf

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -22,3 +22,24 @@ output "check_db_available_arn" {
     value = try({for k, v in aws_lambda_function.main : k => v.arn
                   if lookup( v.tags, "Name", "") == "check-db-available"}, null)   
 }
+
+output "validate_db_writable_arn" {
+    value = try({for k, v in aws_lambda_function.main : k => v.arn
+                  if lookup( v.tags, "Name", "") == "validate-db-writable"}, null)   
+}
+
+output "scaleup_dr_service_arn" {
+    value = try({for k, v in aws_lambda_function.main : k => v.arn
+                  if lookup( v.tags, "Name", "") == "scaleup-dr-service"}, null)   
+}
+
+output "check_ecs_healthy_arn" {
+    value = try({for k, v in aws_lambda_function.main : k => v.arn
+                  if lookup( v.tags, "Name", "") == "check-ecs-healthy"}, null)   
+}
+
+output "validate_application_arn " {
+    value = try({for k, v in aws_lambda_function.main : k => v.arn
+                  if lookup( v.tags, "Name", "") == "validate-application"}, null)   
+}
+
