d6155346855468cda89ebe5fbb837372e683c3c5
qays-test <qaysalnajjad317@gmail.com>
2026-04-17 16:29:22 +0400

    Update outputs.tf

--- environments/dr/read_replica_rds/outputs.tf
+++ environments/dr/read_replica_rds/outputs.tf
@@ -2,6 +2,10 @@ output "wordpress_secret_arn" {
     value = aws_secretsmanager_secret.rr.arn
 }
 
-output "dr_rds_sg" {
-    value = module
+output "dr_rds_sg_id" {
+    value = module.sg.dr_rds_sg_id
+}
+
+output "dr_secret_manager_endpoint_sg_id" {
+    value = module.sg.dr_secret_manager_endpoint_sg_id
 }
