3166fee3036f68e6b6ce41de674e32681889fb5c
qays-test <qaysalnajjad317@gmail.com>
2026-04-19 01:22:09 +0400

    Change RDS password to use random_password resource

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -28,7 +28,7 @@ resource "aws_db_instance" "rds" {
   storage_encrypted = false
   username = random_password.db.result
   manage_master_user_password = false
-  password = var.rds.password
+  password = random_password.db
   db_name = var.rds.db_name
   backup_retention_period = 7
   skip_final_snapshot = true
