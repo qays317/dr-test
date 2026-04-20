507cbba74a5e0de2600e7fde8bad0b3975138c91
qays-test <qaysalnajjad317@gmail.com>
2026-04-20 12:46:45 +0400

    Update stacks_config.sh

--- scripts/deployment-automation-scripts/stacks_config.sh
+++ scripts/deployment-automation-scripts/stacks_config.sh
@@ -27,9 +27,8 @@ STACK_VARS["primary/rds"]="\
   -var rds_identifier=$RDS_IDENTIFIER \
   -var primary_region=$PRIMARY_REGION \
   -var state_bucket_name=$TF_STATE_BUCKET_NAME \
-  -var state_bucket_region=$TF_STATE_BUCKET_REGION \
-  -var-file=rds.tfvars" 
-
+  -var state_bucket_region=$TF_STATE_BUCKET_REGION 
+  
 # DR NETWORK
 STACK_VARS["dr/network"]="\
   -var dr_region=$DR_REGION \
